577433dc855f5b8c08965664511db02de60b4b2e
ZippyMagician <zippymagician1@gmail.com>
2018-11-11 18:19:10 -0500

    Fix test error

--- test/ziptest.js
+++ test/ziptest.js
@@ -2,7 +2,9 @@ const ast = require('../src/lexer.js');
 const parse = require('../src/parser.js');
 const fs = require('fs');
 
-fs.readFile('test.zp', 'utf8', (err, data) => {
+console.log("Beginning build");
+
+fs.readFile('test/test.zp', 'utf8', (err, data) => {
     var code = data;
     parse(ast(code));
 })
