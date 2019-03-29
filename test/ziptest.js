c1d216dd6a26cd43fedc28b55cc7652494e4b61b
ZippyMagician <zippymagician1@gmail.com>
2019-03-29 07:32:37 -0400

    Fix syntax change
    ziplang.tokenify => ziplang.tokenstream

--- test/ziptest.js
+++ test/ziptest.js
@@ -5,5 +5,5 @@ console.log("Beginning build");
 
 fs.readFile('test/test.zp', 'utf8', (err, data) => {
     var code = data;
-    ziplang.execute(ziplang.tokenify(code));
+    ziplang.execute(ziplang.tokenstream(code));
 })
